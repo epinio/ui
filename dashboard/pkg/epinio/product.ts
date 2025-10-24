84da8a0e1cd7b61646578c343f4e79db371001c3
David Johnson <dcharles525@gmail.com>
2025-10-24 11:06:19 -0400

    test(remove overriding references that are not needed):

--- dashboard/pkg/epinio/product.ts
+++ dashboard/pkg/epinio/product.ts
@@ -3,10 +3,9 @@ import { EPINIO_PRODUCT_NAME} from "./types";
 
 export function init($plugin: IPlugin, store: any) {
   const BLANK_CLUSTER = '_';
-  const YOUR_K8S_RESOURCE_NAME = 'provisioning.cattle.io.cluster';
   const VIEW_RESOURCE_PAGE_NAME = 'View Resource';
 
-  const { product, configureType, virtualType, basicType } = $plugin.DSL(store, EPINIO_PRODUCT_NAME);
+  const { product, virtualType, basicType } = $plugin.DSL(store, EPINIO_PRODUCT_NAME);
 
   product({
     icon: 'gear',
@@ -23,23 +22,7 @@ export function init($plugin: IPlugin, store: any) {
     },
   });
 
-  // defining a k8s resource as page
-  configureType(YOUR_K8S_RESOURCE_NAME, {
-    displayName: 'some-custom-name-you-wish-to-assign-to-this-resource',
-    isCreatable: true,
-    isEditable:  true,
-    isRemovable: true,
-    showAge:     true,
-    showState:   true,
-    canYaml:     true,
-    customRoute: {
-      name:   `${ EPINIO_PRODUCT_NAME }-c-cluster-view-resource`,
-      params: {
-        product:  EPINIO_PRODUCT_NAME,
-        resource: YOUR_K8S_RESOURCE_NAME
-      }
-    }
-  });
+
 
   // creating a custom page
   virtualType({
@@ -52,6 +35,6 @@ export function init($plugin: IPlugin, store: any) {
   });
 
   // registering the defined pages as side-menu entries
-  basicType([YOUR_K8S_RESOURCE_NAME, VIEW_RESOURCE_PAGE_NAME]);
+  basicType([VIEW_RESOURCE_PAGE_NAME]);
 
 }
