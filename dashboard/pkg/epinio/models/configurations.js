6ac0780635962a35e732683f6c93a74976bd9b3c
David Johnson <dcharles525@gmail.com>
2026-04-07 10:19:22 -0400

    Update data structure in configurations.js

--- dashboard/pkg/epinio/models/configurations.js
+++ dashboard/pkg/epinio/models/configurations.js
@@ -81,7 +81,7 @@ export default class EpinioConfigurationModel extends EpinioNamespacedResource {
         'content-type': 'application/json',
         accept:         'application/json'
       },
-      data: { ...this.data }
+      data: { data: { ...this.data } }
     });
   }
 
